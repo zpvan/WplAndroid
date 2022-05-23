2850827c0835e7e287799ddbcc2b68ab655a7ada
knox <843762968@qq.com>
2022-05-24 00:18:58 +0800

    日常更新

---
+++ leetcode/src/test/java/com/knox/leetcode/number/Test965.java
@@ -0,0 +1,27 @@
+package com.knox.leetcode.number;
+
+public class Test965 {
+
+	public boolean isUnivalTree(TreeNode root) {
+        
+	}
+
+	private class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
