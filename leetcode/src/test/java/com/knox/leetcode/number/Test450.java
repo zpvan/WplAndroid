aeb3f5081f924bfac10f27ef9a64602cc65ed700
knox <843762968@qq.com>
2022-06-05 09:55:18 +0800

    日常更新

--- leetcode/src/test/java/com/knox/leetcode/number/Test450.java
+++ leetcode/src/test/java/com/knox/leetcode/number/Test450.java
@@ -3,9 +3,10 @@ package com.knox.leetcode.number;
 class Test450 {
 
 
-	public TreeNode deleteNode(TreeNode root, int key) {
+	public TreeNode deleteNode2(TreeNode root, int key) {
 		if (root == null) return null;
 
+		TreeNode prev = null;
 		TreeNode curr = root;
 		while (curr != null) {
 			if (curr.val == key) {
@@ -43,13 +44,55 @@ class Test450 {
 						curr.val = t.val;
 						p.right = null;
 					}
+				} else {
+					// 删掉
+					if (prev == null) return null;
+					if (prev.val > key) prev.left = null;
+					if (prev.val < key) prev.right = null;
 				}
+				break;
 			} else {
-				if (key < curr.val) curr = curr.left;
-				if (key > curr.val) curr = curr.right;
+				prev = curr;
+				int cval = curr.val;
+				if (key < cval) curr = curr.left;
+				if (key > cval) curr = curr.right;
 			}
 		}
 
+		return root;
+	}
+
+	public TreeNode deleteNode(TreeNode root, int key) {
+		if (root == null) {
+			return null;
+		}
+		if (root.val > key) {
+			root.left = deleteNode(root.left, key);
+			return root;
+		}
+		if (root.val < key) {
+			root.right = deleteNode(root.right, key);
+			return root;
+		}
+		if (root.val == key) {
+			if (root.left == null && root.right == null) {
+				return null;
+			}
+			if (root.right == null) {
+				return root.left;
+			}
+			if (root.left == null) {
+				return root.right;
+			}
+			TreeNode successor = root.right;
+			while (successor.left != null) {
+				successor = successor.left;
+			}
+			root.right = deleteNode(root.right, successor.val);
+			successor.right = root.right;
+			successor.left = root.left;
+			return successor;
+		}
 		return root;
 	}
 
--- leetcode/src/test/java/com/knox/leetcode/number/Test473.java
+++ leetcode/src/test/java/com/knox/leetcode/number/Test473.java
@@ -2,16 +2,45 @@ package com.knox.leetcode.number;
 
 import org.junit.Test;
 
-class Test473 {
+import java.util.Arrays;
+
+public class Test473 {
 
 	@Test
 	public void test() {
-		System.out.println("ans: " + makesquare(new int[]{1, 1, 2, 2, 2}));
-		System.out.println("ans: " + makesquare(new int[]{3, 3, 3, 3, 4}));
+		System.out.println("ans: " + makesquare(new int[]{1, 1, 2, 2, 2})); // true
+		System.out.println("ans: " + makesquare(new int[]{3, 3, 3, 3, 4})); // false
 	}
 
+	int[] ms;
+	int sideLength;
 	public boolean makesquare(int[] matchsticks) {
+		// DFS + 剪枝
 
+		int sum = Arrays.stream(matchsticks).sum();
+		// 不能乘除4, 返回false
+		if ((sum & 0x03) != 0) return false;
+		sideLength = sum / 4;
+
+		Arrays.sort(matchsticks); // 从小到大排序
+		ms = matchsticks;
+		return dfs(matchsticks.length - 1, new int[4]);
+	}
+
+	private boolean dfs(int idx, int[] square) {
+		// 当前这轮用 ms[idx], 分别试试4条边
+
+		// 如果用完最后一根火柴, 表示刚好能拼成一个正方行, 因为拼的时候有剪枝
+		if (idx == -1) return true;
+
+		for (int i = 0; i < 4; i++) {
+			square[i] += ms[idx];
+			// 剪枝
+			if (square[i] <= sideLength && dfs(idx - 1, square)) {
+				return true;
+			}
+			square[i] -= ms[idx];
+		}
 		return false;
 	}
 }
