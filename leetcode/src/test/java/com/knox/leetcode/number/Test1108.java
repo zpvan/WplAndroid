9226ccbc7560a3eab580372d941c68883569d8e6
knox <843762968@qq.com>
2022-06-21 00:04:17 +0800

    日程更新

---
+++ leetcode/src/test/java/com/knox/leetcode/number/Test1108.java
@@ -0,0 +1,23 @@
+package com.knox.leetcode.number;
+
+import org.junit.Test;
+
+public class Test1108 {
+
+   @Test
+   public void test() {
+      System.out.println("ans: " + defangIPaddr("1.1.1.1"));
+   }
+
+   public String defangIPaddr(String address) {
+      StringBuilder sb = new StringBuilder();
+      for (Character c : address.toCharArray()) {
+         if (c.equals('.')) {
+            sb.append("[.]");
+         } else {
+            sb.append(c);
+         }
+      }
+      return sb.toString();
+   }
+}
--- leetcode/src/test/java/com/knox/leetcode/offer/Test029.java
+++ leetcode/src/test/java/com/knox/leetcode/offer/Test029.java
@@ -3,16 +3,53 @@ package com.knox.leetcode.offer;
 public class Test029 {
 
 	public Node insert(Node head, int insertVal) {
-       if (head == null) {
-		   // 空值
-		   return new Node(insertVal);
-	   }
+		if (head == null) {
+			// 空值
+			head = new Node(insertVal);
+			head.next = head;
+			return head;
+		}
 
-	   Node curr = head;
-	   while (curr.next != null) {
-		   Node next = curr.next;
-		   
-	   }
+		if (head.next == head) {
+			// 单值
+			head.next = new Node(insertVal, head);
+			return head;
+		}
+
+		Node curr = head;
+		boolean loop = false;
+		int min = 0x3f3f3f;
+		Node firstMin = null;
+		// 大于等于两个值
+		while (curr.next != null) {
+			Node next = curr.next;
+			if (curr.val <= min && !loop) {
+				// 循环第一圈时, 找到最小值, 同时找第一个最小值
+				min = curr.val;
+				firstMin = curr;
+			}
+			if (next.val < curr.val) {
+				// 补充找第一个最小值
+				firstMin = next;
+			}
+			if (next.val >= insertVal && curr.val <= insertVal) {
+				// 十分正常的情况
+				curr.next = new Node(insertVal, next);
+				break;
+			}
+			if (loop && next == firstMin) {
+				// 循环一圈之后, 没从十分正常的情况退出循环, 则找到一个最小值与最大值之间退出循环
+				curr.next = new Node(insertVal, next);
+				break;
+			}
+			curr = next;
+			if (curr == head) {
+				// 循环一圈时, 添加标记
+				loop = true;
+			}
+		}
+
+		return head;
 	}
 
 	private class Node {
