4586318b15915288f0d9ed29bf534120d5707b48
knox <843762968@qq.com>
2022-06-26 23:47:03 +0800

    日常更新

--- leetcode/src/test/java/com/knox/leetcode/number/Test32.java
+++ leetcode/src/test/java/com/knox/leetcode/number/Test32.java
@@ -34,10 +34,9 @@ class Test32 {
 			if (s.charAt(i) == ')') {
 				if (s.charAt(i - 1) == '(') {
 					dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-				}
-			} else {
-				// s.charAt(i) == '('
-				if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+				} else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+					// "....))"
+					
 					dp[i] = dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
 				}
 			}
