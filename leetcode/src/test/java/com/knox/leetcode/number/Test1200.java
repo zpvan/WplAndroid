78a61452d817199b75ac0ca957f280047faed973
knox <843762968@qq.com>
2022-07-04 01:21:00 +0800

    日常更新

---
+++ leetcode/src/test/java/com/knox/leetcode/number/Test1200.java
@@ -0,0 +1,20 @@
+package com.knox.leetcode.number;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Test1200 {
+
+
+	public List<List<Integer>> minimumAbsDifference(int[] arr) {
+		Arrays.sort(arr);
+
+		Map<Integer, List<List<Integer>>> map = new HashMap<>();
+		int minAbs = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length - 1; i++) {
+			
+		}
+	}
+}
